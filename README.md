1285f7c97b8163bb8adc4849c3f4453440b46b72
Johnson Lai <jlwhoo7@gmail.com>
2017-05-25 08:18:42 +0800

    done johnny-five part is first

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Learning node.js for the sake of node red
 ## USING ESP8266 I NEED TO LEARN THIS AND MONGO DB
+
+
+### Reference
+[johnny-five](http://johnny-five.io/)
---
+++ johnny-five/first/first.js
@@ -0,0 +1,16 @@
+// Johnny-Five
+var five = require("johnny-five");
+var board = new five.Board();
+
+
+board.on("ready", function() {
+  var led = new five.Led(13);
+
+	  // This will grant access to the led instance
+	  // from within the REPL that's created when
+	  // running this program.
+	  this.repl.inject({
+	    led: led
+	  });
+
+});
