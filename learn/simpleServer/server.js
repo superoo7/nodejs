94f862d759de5783b0b73aa7b1e0e2af1a9fe16e
Johnson Lai <jlwhoo7@gmail.com>
2017-05-31 07:27:20 +0800

    learning node.js

--- learn/simpleServer/server.js
+++ learn/simpleServer/server.js
@@ -1,8 +1,10 @@
+// http://debuggable.com/posts/understanding-node-js:4bd98440-45e4-4a9a-8ef7-0f7ecbdd56cb
+
 var http = require("http");
 
 http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("<!DOCTYPE html><body><h1>Hello World</h1></body></html>");
+	response.write("Hello World");
 	response.end();
 
-}).listen(8000);
+}).listen(8888);
